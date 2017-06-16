6901a3ded854b5a1ce67fc18f4da883125542926
arcsinW <wangx86@live.com>
2017-06-16 08:06:46 +0800

    add missed files

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,56 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    private string[,] map = new string [15,15];
+    
+    public GameObject mapContainer;
+    public GameObject mapCudePrefab;
+    public GameObject roadCubePrefab;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        string path = Application.streamingAssetsPath + "/" + "map.txt";
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                string line = reader.ReadLine();
+                string[] tmp = line.Split(' ');
+                for (int j = 0; j < 15; j++)
+                {
+                    map[i,j] = tmp[j];
+                }
+            } 
+        }
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    GameObject tmpPrefab;
+                    Vector3 transform = new Vector3(-(70 - j * 5), 2, - (-70 + i * 5));
+
+                    if (map[i,j].Equals("0")) //Map
+                    {
+                        tmpPrefab = GameObject.Instantiate(mapCudePrefab, transform, mapContainer.transform.rotation);
+                        tmpPrefab.transform.localScale = new Vector3(4, 1, 4);
+                        tmpPrefab.transform.parent = mapContainer.transform;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TextReaderHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TextReaderHelper
+    {
+        public static string ReadText(string filePath)
+        {
+            string path = Application.streamingAssetsPath + "/" + filePath;
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string text = reader.ReadToEnd();
+                return text;
+            }
+        }
+
+        public static string ReadLine(string filePath)
+        {
+            string path = Application.streamingAssetsPath + "/" + filePath;
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string text = reader.ReadLine();
+                return text;
+            }
+        }
+    }
+}
